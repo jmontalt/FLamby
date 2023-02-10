6027761bd021b4b04c37b6b32f0e072fa42abec3
Javier Montalt Tordera <javier.monttalttordera@bayer.com>
2023-02-10 11:47:15 +0000

    Add script for federated benchmarking of Cam16

---
+++ tools/benchmark_camelyon16_fed.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+# Usage:
+#
+#   ./tools/benchmark_camelyon16_fed.sh
+#
+# NOTE: Run this AFTER downloading with ./tools/download_camelyon16.sh
+# and preprocessing with ./tools/preprocess_camelyon16.sh.
+set -e
+
+WORKDIR=/workspace
+
+for SEED in 42 # 43 44 45 46
+do
+    ./tools/run_in_docker.sh python \
+        $WORKDIR/flamby/benchmarks/fed_benchmark.py \
+        --seed $SEED \
+        -cfp \
+        $WORKDIR/flamby/config_camelyon16.json
+done
